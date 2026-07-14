2f16b09d4474b86a04698e37e734eca6d07eedb4
Ayush Pekamwar <ayushpekamwar@gmail.com>
2026-07-14 10:35:22 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main()
-{
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
 
-  while (1)
-  {
-    cout << "$ ";
-    string userInput;
-    getline(cin, userInput);
-    cout << userInput << ": command not found" << endl;
-  }
+bool shouldTerminate(string &userInput) {
+    return userInput == "exit";
+}
+
+int main() {
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    while (1) {
+        cout << "$ ";
+        string userInput;
+        getline(cin, userInput);
+
+        if (shouldTerminate(userInput)) {
+            return 0;
+        }
+        cout << userInput << ": command not found" << endl;
+    }
 }
