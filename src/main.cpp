380bec960b4fcc6bf6f16780d38a1f02b5015152
Ayush Pekamwar <ayushpekamwar@gmail.com>
2026-07-14 10:06:20 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   cout << "$ ";
+  string userInput;
+  getline(cin, userInput);
+  cout << userInput << ": command not found" << endl;
 }
