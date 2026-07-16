9ceaae0352f07dabbef233e11d277a991e90673c
Ayush Pekamwar <ayushpekamwar@gmail.com>
2026-07-16 18:04:26 +0530

    added double quotes handling (without special chars for now) to parser

--- src/Parser/parser.cpp
+++ src/Parser/parser.cpp
@@ -9,17 +9,23 @@ using namespace std;
 std::vector<std::string> parser(const std::string &input) {
 
     std::vector<std::string> tokens;
+    //state
     std::string curToken;
     bool isSingleQuote = false;
+    bool isDoubleQuote = false;
 
     for(const char& c : input){
-        if(c=='\''){
+        if(c=='\"' && !isSingleQuote){
+            isDoubleQuote^=1;
+            continue;
+        }
+        if(c=='\'' && !isDoubleQuote){
             // toggle the state 
             isSingleQuote ^= 1;
             continue;
         }
 
-        if(c == ' ' && !isSingleQuote){
+        if(c == ' ' && !isSingleQuote && !isDoubleQuote){
             if(!curToken.empty()) tokens.push_back(curToken);
             curToken.clear();
         }
